3dcf18b0aca20b50246922b8c3e3c6f71b57595b
Miao-Lisa <142939954+Miao-Lisa@users.noreply.github.com>
2024-09-20 17:49:41 +0000

    Attempt 2

---
+++ irs.py
@@ -0,0 +1,10 @@
+class InformationRetrievalSystem:
+    
+    def __init__(self, filename):
+        self.filename = filename
+    def read(self):
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            #read into a list
+            data = list(reader)
+            return data
